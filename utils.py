dcd48b69ffd92c54462e4057ccf30790d5b0eb70
Patricio RIffo <patricionicolas@live.cl>
2020-05-09 16:11:42 -0400

    Fix: Se agrega
    La funcion para graficar en graficos tipo h y se agrega un texto si se desea pasar por parametro.

--- utils.py
+++ utils.py
@@ -1,11 +1,24 @@
 import matplotlib.pyplot as plt
 
-def labelon(X, Y, xx, yy, size, color, weight):
+def labelon(X, Y, xx, yy, size, color, weight, txt = ''):
+    n = len(X)
+    for i in range (n):
+        number = '{:,}'.format(int(Y[i])).replace(',', '.')
+        number = str(number) + str (txt)
+        plt.annotate(
+            number, xy=(X[i], Y[i]),
+            xytext =(0 + xx, 0 + yy),
+            textcoords="offset points",
+            ha ='center', va='bottom', 
+            color  = color, size = size, 
+            weight = weight)
+
+def labelon_h(X, Y, xx, yy, size, color, weight):
     n = len(X)
     for i in range (n):
         number = '{:,}'.format(int(Y[i])).replace(',', '.')
         plt.annotate(
-            number, xy=(X[i], Y[i]),
+            number, xy=(Y[i], X[i]),
             xytext =(0 + xx, 0 + yy),
             textcoords="offset points",
             ha ='center', va='bottom', 
